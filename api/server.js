2684a424614b129ce6186f07a627b23cd578f092
Israel Arvizu <israelarvizuofficial@gmail.com>
2023-03-27 16:36:47 -0700

    Mongo DB Atlas working and server running smooth

--- api/server.js
+++ api/server.js
@@ -8,11 +8,21 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
-console.log(process.env.URI, "key")
-
 mongoose.connect(process.env.URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log("Connected to Database")).catch(console.error);
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+const User = require('./models/user');
+
+app.get('/user', async (req, res)=> {
+    const user = await User.find();
+
+    res.json(User)
+})
+
+app.post('/', (req, res) =>{
+    res.json("Successfull Post")
+})
+
+app.listen(3005, () => console.log("Server started on port 3005"));
